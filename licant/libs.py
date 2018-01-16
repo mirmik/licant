a7a087d4874bd8f126322ab831b307c2da8723e9
mirmik <mirmikns@yandex.ru>
2018-01-16 16:58:40 +0300

    a

--- licant/libs.py
+++ licant/libs.py
@@ -9,6 +9,11 @@ lpath = os.path.expanduser("~/.config/licant")
 
 libs = None
 
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
+
 def include(lib):
 	global libs
 	if libs == None:
@@ -21,7 +26,7 @@ def include(lib):
 		if os.path.exists(lpath):
 			llibs = json.load(open(lpath))
 
-		libs = {**glibs, **llibs}
+		libs =  merge_two_dicts(glibs, llibs)
 
 	if not lib in libs:
 		print("Unregistred library")
