f977633569bc8044fbb9d7b4611416b19fdac837
Mirmik <mirmikns@yandex.ru>
2018-01-18 12:21:48 +0300

    coretest

--- licant/core.py
+++ licant/core.py
@@ -15,12 +15,34 @@ class licantCore:
 		parser.add_option("-d", "--debug", action="store_true", default=False, 
 			help="print full system commands")
 		parser.add_option("-j", "--threads", default=1, help="amount of threads for executor")
-
-		#self.runtime["threads"] = int(argv["threads"])
 		opts = parser.parse_args(argv)
 		return opts
-		#print(options)
-		#print(args)
+
+	def add_target(self, tgt, deps=[], **kwargs):
+		self.targets[tgt] = Target(tgt=tgt, deps=deps, **kwargs)
+
+	def subtree(self, root):
+		return SubTree(self, root)
+
+	def get_target(self, tgt):
+		if tgt in self.targets:
+			return self.targets[tgt]
+		print("Unregistred target: {0}".format(tgt))
+		exit(-1)
+	
+	def depends_as_set(self, tgt, incroot=True):
+		res = set()
+		if incroot:
+			res.add(tgt)
+		
+		target = self.get_target(tgt)
+		
+		for d in target.depends:
+			if not d in res:
+				res.add(d)
+				subres = self.depends_as_set(d)
+				res = res.union(subres)
+		return res
 
 core = licantCore()
 
@@ -34,6 +56,8 @@ class Target:
 		
 		for k, v in kwargs.items():
 			setattr(self, k, v)
+
+		self.deps = self.depends
 
 	def invoke(self, func, **kwargs):
 		if (isinstance(func, str)):
@@ -55,7 +79,6 @@ def get_target(tgt):
 	if tgt in core.targets:
 		return core.targets[tgt]
 	print("Unregistred target: {0}".format(tgt))
-	#traceback.print_stack()
 	exit(-1)
 
 def depends_as_set(tgt, incroot=True):
@@ -78,9 +101,10 @@ def invoke_foreach(func):
 		v.invoke(func)
 
 class SubTree:
-	def __init__(self, root):
+	def __init__(self, core, root):
 		self.root = root
-		self.depset = depends_as_set(root)
+		self.core = core
+		self.depset = core.depends_as_set(root)
 
 	def update(self):
 		SubTree.__init__(self, root)
@@ -90,7 +114,7 @@ class SubTree:
 		ret = None
 
 		for d in self.depset:
-			target = get_target(d)
+			target = self.core.get_target(d)
 			if cond==None:
 				ret = target.invoke(ops)
 			else:
@@ -109,12 +133,12 @@ class SubTree:
 	
 		for t in targets:
 			for dname in t.depends:
-				dtarget = get_target(dname)
+				dtarget = self.core.get_target(dname)
 				dtarget.rdepends.append(t.tgt)
 	
 	
-	def reverse_recurse_invoke_single(self, ops, cond=licant.util.always_true):
-		targets = [get_target(t) for t in self.depset]
+	def reverse_recurse_invoke_single(self, ops, threads=None, cond=licant.util.always_true):
+		targets = [self.core.get_target(t) for t in self.depset]
 		sum = 0
 	
 		self.__generate_rdepends_lists(targets)
@@ -124,7 +148,7 @@ class SubTree:
 		for t in targets:
 			if t.rcounter == len(t.depends):
 				works.put(t)
-	
+
 		while(not works.empty()):
 			w = works.get()
 	
@@ -136,7 +160,7 @@ class SubTree:
 				if ret == 0:
 					sum += 1
 	
-			for r in [get_target(t) for t in w.rdepends]:
+			for r in [self.core.get_target(t) for t in w.rdepends]:
 				r.rcounter = r.rcounter + 1
 				if r.rcounter == len(r.depends):
 					works.put(r)
@@ -144,7 +168,7 @@ class SubTree:
 		return sum
 
 	def reverse_recurse_invoke_threads(self, ops, threads, cond=licant.util.always_true):
-		targets = [get_target(t) for t in self.depset]
+		targets = [self.core.get_target(t) for t in self.depset]
 		
 		self.__generate_rdepends_lists(targets)
 		works = licant.util.queue()
@@ -207,12 +231,23 @@ class SubTree:
 			exit(-1)	
 		return info.sum
 
-	def reverse_recurse_invoke(self, threads = 1, *args, **kwargs):
-		if threads == 1:
-			return self.reverse_recurse_invoke_single(*args, **kwargs)
+	def reverse_recurse_invoke(self, *args, **kwargs):
+		if "threads" in kwargs:
+			if kwargs["threads"] == 1:
+				return self.reverse_recurse_invoke_single(*args, **kwargs)
+			else:
+				return self.reverse_recurse_invoke_threads(*args, **kwargs)
 		else:
-			return self.reverse_recurse_invoke_threads(threads = threads, *args, **kwargs)
-				
+			return self.reverse_recurse_invoke_single(*args, **kwargs)
+
+	def __str__(self):
+		ret = ""
+		for d in sorted(self.depset):
+			t = self.core.get_target(d)
+			s = "{}: {}\n".format(d, sorted(t.deps))
+			ret += s
+		ret = ret[:-1]
+		return ret
 
 def subtree(root):
-	return SubTree(root)
+	return SubTree(core, root)
---
+++ test/core/core.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import os
+from licant.core import *
+
+core = licantCore()
+
+def hello_world_0(tgt):
+	print("hw0")
+
+def hello_world_1(tgt):
+	print("hw1")
+
+def copy(tgt):
+	task = "cp {} {}".format(tgt.src, tgt.tgtpath)
+	print(task)
+	res = os.system(task)
+
+core.add_target(tgt="copy", src="a", tgtpath="b", deps=[], do=copy)
+core.add_target(tgt="hello_world_0", deps=[], do=hello_world_0)
+core.add_target(tgt="hello_world_1", deps=["hello_world_0", "copy"], do=hello_world_1)
+
+stree = core.subtree("hello_world_1")
+
+print(stree)
+
+stree.reverse_recurse_invoke("do", threads = 1)
+
