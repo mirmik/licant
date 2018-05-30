a0a5a24d243b7270e2e4af41336a69c3e45d0a11
mirmik <mirmikns@yandex.ru>
2018-05-30 21:34:35 +0300

    a

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 setup(
 	name = 'licant',
 	packages = ['licant'],
-	version = '0.17.0',
+	version = '0.17.1',
 	license='MIT',
 	description = 'licant make system',
 	author = 'Sorokin Nikolay',
