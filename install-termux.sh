79b85226e8aa7d6e82472ddaa3406cc54a23d968
mirmik <mirmikns@yandex.ru>
2018-10-08 00:55:23 +0300

    removed install script

--- install-termux.sh
+++
@@ -1,6 +0,0 @@
-DIR=$PREFIX/lib/python3.6/licant
-
-mkdir -p $DIR/
-cp -r licant/* $DIR/
-#cp configurator/licant-config.py 
-#/usr/bin/licant-config
--- install.sh
+++
@@ -1,5 +0,0 @@
-DIR=/usr/lib/python3.5/licant
-
-mkdir -p $DIR/
-cp -r licant/* $DIR/
-cp configurator/licant-config.py /usr/bin/licant-config
