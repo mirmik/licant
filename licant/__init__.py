c11a940763b716545a42eb35d966b344bb2f1151
mirmik <mirmikns@yandex.ru>
2019-06-20 11:26:55 +0300

    'a'

--- licant/__init__.py
+++ licant/__init__.py
@@ -19,7 +19,7 @@ from licant.util import error
 import licant.scripter
 from licant.libs import include
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 def directory():
     return licant.scripter.scriptq.curdir()
