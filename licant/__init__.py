60e20a1018abfba9c7239f583181d3c2e8c46061
netricks <netricks@protonmail.com>
2020-08-13 16:27:12 +0300

    'git'

--- licant/__init__.py
+++ licant/__init__.py
@@ -22,7 +22,7 @@ from licant.util import error
 import licant.scripter
 from licant.libs import include
 
-__version__ = "1.7.1"
+__version__ = "1.8.1"
 
 def directory():
     return licant.scripter.scriptq.curdir()
