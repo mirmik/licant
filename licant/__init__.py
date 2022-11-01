f254ef467f942c2d5c13103ad3946875c6bdb731
mirmik <mirmikns@yandex.ru>
2022-11-01 19:09:39 +0300

    'next version'

--- licant/__init__.py
+++ licant/__init__.py
@@ -22,7 +22,7 @@ from licant.cxx_make import gcc_toolchain, clang_toolchain, host_toolchain
 import licant.scripter
 from licant.libs import include
 
-__version__ = "1.13.2"
+__version__ = "1.13.3"
 
 
 def directory():
